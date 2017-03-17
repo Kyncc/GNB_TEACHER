0eae5d97a0faf7fa3d57efce9d9a0721350a6912
Hope <hopefullstack@gmail.com>
2017-03-17 15:29:24 +0800

    no message

---
+++ src/features/workbookByClass/pages/exercise/answer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <p>asdasdasdasasd
+      </p>
+  </div>
+
+</template>
+
+<script>
+  import {Group} from 'vux'
+  import { mapActions,mapGetters} from 'vuex'
+
+  export default {
+    components:{
+      Group
+    },
+    methods:{
+      
+    },
+    computed:{
+      ...mapGetters(['path','Params'])
+    }
+  }
+</script>
